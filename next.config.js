84035891d3f664fee5482b70bc77b85be0517dcc
Louis Petrik <33809479+LouisPetrik@users.noreply.github.com>
2021-07-17 21:24:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   webpack(config) {
     config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
+    config.experiments = { asyncWebAssembly: true }
     return config
   },
 }
